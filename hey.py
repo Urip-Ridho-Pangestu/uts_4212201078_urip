3cc2ed887aa0411d4e9273d17decbc78b33bc736
Urip Ridho Pangestu <ekoprasetiyo13121984@gmail.com>
2024-11-01 23:05:28 +0700

    kedua

--- hey.py
+++ hey.py
@@ -21,7 +21,7 @@ dt = test_data.drop(0, axis=1).head(4000)
 print(d.shape)
 print(l.shape)
 print(dt.shape)
-print(lt.shape)
+
 
 class_namess = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
